271dd4c35a181ec3d627a22e875660f0a89cd9a5
Vova MAD <vovamad@iMac-Chief.local>
2020-10-08 16:36:15 +0300

    TESTMAC

---
+++ app/src/main/java/com/example/test/RVAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.test
+
+import android.icu.text.Transliterator
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.rv_view.view.*
+
+class RVAdapter : RecyclerView.Adapter<RVAdapter.AdapterView>(){
+
+
+
+    val list = listOf("1", "2", "3", "4", "5", "6")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterView {
+        return AdapterView(
+            LayoutInflater.from(parent.context).inflate(R.layout.rv_view, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = 6
+
+
+    override fun onBindViewHolder(holder: AdapterView, position: Int) {
+        holder.itemView.text.text = list[position]
+    }
+
+    class AdapterView (view: View) : RecyclerView.ViewHolder(view){
+
+    }
+}
